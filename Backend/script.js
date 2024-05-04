5fcbdb7644086f0c950c4fd03db0dd12026feb5f
Dan Luca Gutierrez <danlucagutierrez@hotmail.com>
2024-05-04 19:26:28 -0300

    ADD DOM descriptor reading logic without html

--- Backend/script.js
+++ Backend/script.js
@@ -1,19 +1,33 @@
-const MODEL_URL = '/Backend/models';
+const { Canvas, Image, ImageData } = require('canvas');
+const faceapi = require('face-api.js');
+const { loadImage } = require('canvas');
 
-(async () => {
-    await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-    await faceapi.loadFaceLandmarkModel(MODEL_URL)
-    await faceapi.loadFaceRecognitionModel(MODEL_URL)
+// Define la ruta del modelo
+const MODEL_URL = '../Backend/models';
 
-    const image = document.getElementById('image');
-    const canvas = document.getElementById('canvas');
+// Configura face-api.js para usar node-canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
-    let fullFaceDescriptions = await faceapi.detectAllFaces(image)
-        .withFaceLandmarks()
-        .withFaceDescriptors()
+// Carga los modelos necesarios
+async function loadModels() {
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
+    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
+    await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
+}
 
-    faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
-    faceapi.draw.drawFaceLandmarks(canvas, fullFaceDescriptions);
-    console.log(fullFaceDescriptions)
-    
-})();
+// Función para detectar rostros en una imagen
+async function detectFaces(imagePath) {
+    const image = await loadImage(imagePath);
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+    return detections;
+}
+
+// Ruta de la imagen
+const imagePath = '../Backend/images/breaking_bad.';
+
+// Carga los modelos y detecta los rostros en la imagen
+export async function runFR() {
+    await loadModels();
+    const detections = await detectFaces(imagePath);
+    console.log(detections);
+}
