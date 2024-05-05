ecf7df70ad0d8ac20d414a0b024275556b889779
Ruminini <63514002+Ruminini@users.noreply.github.com>
2024-05-05 10:55:53 -0300

    FEAT: Server endpoint Also switched from 'face-api.js' to the more updated '@vladmandic/face-api' which is working with tfjs-node improving detection speeds from 10-20s to 300-1000ms

--- Backend/script.js
+++ Backend/script.js
@@ -1,33 +1,62 @@
 const { Canvas, Image, ImageData } = require('canvas');
-const faceapi = require('face-api.js');
+const tf = require('@tensorflow/tfjs-node');
+const faceapi = require('@vladmandic/face-api');
 const { loadImage } = require('canvas');
-
-// Define la ruta del modelo
-const MODEL_URL = '/Backend/models';
+const fs = require('fs');
 
 // Configura face-api.js para usar node-canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
-// Carga los modelos necesarios
+// Define la ruta del modelo
+const MODEL_URL = './models';
+
+// Carga los modelos
 async function loadModels() {
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
+    faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
+    faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
+    faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
 }
 
-// Función para detectar rostros en una imagen
-async function detectFaces(imagePath) {
-    const image = await loadImage(imagePath);
+// Detecta uno o mas rostros en una imagen
+async function detectFaces(base64Image) {
+    const image = await loadImage(base64Image);
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
     return detections;
 }
 
-// Ruta de la imagen
-const imagePath = '/Backend/images/breaking_bad.jpg';
 
-// Carga los modelos y detecta los rostros en la imagen
-export async function runFR() {
+async function main() {
+    await faceapi.tf.setBackend('tensorflow');
+    await faceapi.tf.ready();
     await loadModels();
-    const detections = await detectFaces(imagePath);
-    console.log(detections);
+    
+    runServer();
 }
+
+async function runServer() {
+    const express = require('express');
+    const app = express();
+    const PORT = 3000;
+
+    app.use(express.json({ limit: '10mb' }));
+    app.post('/recognizeFaces', async (req, res) => {
+        const { base64Image } = req.body;
+        if (!base64Image) {
+            return res.status(400).json({ error: 'Missing base64Image field in the request body' });
+        }
+        try {
+            const detections = await detectFaces('data:image/png;base64,'+base64Image);
+            res.json(detections);
+        } catch (error) {
+            console.error('Error processing image:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+
+    // Inicia el servidor
+    app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+}
+
+main();
